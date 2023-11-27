7723bf17b4af938323a8b28da09c5cebe616af24
Christian Mazakas <christian.mazakas@gmail.com>
2023-11-27 11:16:26 -0800

    Initial attempt at GHA CI workflow

---
+++ test/cmake_test/main.cpp
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2023 Christian Mazakas
+//
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+
+#include <boost/buffers/src.hpp>
+
+int main()
+{
+    return 0;
+}
